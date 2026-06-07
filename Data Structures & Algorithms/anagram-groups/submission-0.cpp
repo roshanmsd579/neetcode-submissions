875de4f656e2553cac8b3501be30baa752a111ec
Roshan Kumar Singh <139267981+roshanmsd579@users.noreply.github.com>
2026-06-07 16:28:04 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(auto& s:strs){
+            string key = s;
+            sort(key.begin(),key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>>result;
+        for(auto& [key,group]:mp){
+            result.push_back(group);
+        }
+        return result;
+    }
+};
